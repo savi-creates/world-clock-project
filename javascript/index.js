29f99b00a63a1f22e58938a5ccbfdd052a0de1ac
savi-creates <savannahandresson@gmail.com>
2025-03-26 15:27:17 +0000

    updated js

--- javascript/index.js
+++ javascript/index.js
@@ -10,18 +10,18 @@ function updateTime() {
   // New York
   let newYorkTimeElement = document.querySelector("#time-newyork");
   let newYorkDateElement = document.querySelector("#city-state-newyork");
-  let NewYorkTime = moment().tz("America/New_York");
+  let newYorkTime = moment().tz("America/New_York");
 
-  newYorkDateElement.innerHTML = londonTime.format("ddd, MMM Do YYYY");
-  newYorkTimeElement.innerHTML = londonTime.format("h:mm:ss A");
+  newYorkDateElement.innerHTML = newYorkTime.format("ddd, MMM Do YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format("h:mm:ss A");
 
   // Tokyo
   let tokyoTimeElement = document.querySelector("#time-tokyo");
   let tokyoDateElement = document.querySelector("#city-state-tokyo");
   let tokyoTime = moment().tz("Asia/Tokyo");
 
-  tokyoDateElement.innerHTML = londonTime.format("ddd, MMM Do YYYY");
-  tokyoTimeElement.innerHTML = londonTime.format("h:mm:ss A");
+  tokyoDateElement.innerHTML = tokyoTime.format("ddd, MMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss A");
 }
 
 updateTime();
